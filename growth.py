d1371e1b3d3481b0e16fd11dd33a74b3d5ba8401
Haseena-shah <zebishah1502@gmail.com>
2025-03-08 15:12:12 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -29,7 +29,7 @@ if uploaded_file:
         file_ext = os.path.splitext(file.name)[-1].lower()
         if file_ext == '.csv':
             df = pd.read_csv(file)
-        elif file_ext == ".xlsx":
+        elif file_ext == "xlsx":
             df = pd.read_excel(file)
         else: 
          st.error(f"unsupported file type: {file_ext}")
